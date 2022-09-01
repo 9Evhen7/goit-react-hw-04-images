b7b5b46d7043183f218118b75be30d29639f220c
9Evhen7 <96841920+9Evhen7@users.noreply.github.com>
2022-09-01 17:08:32 +0300

    loadindStyles

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,26 @@
 import { Watch } from 'react-loader-spinner';
+import styled from 'styled-components';
+
+const Wraper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 200px;
+  width: 200px;
+`;
 
 export const Loader = () => {
   return (
-    <Watch
-      height="80"
-      width="80"
-      radius="48"
-      color="#919191"
-      ariaLabel="watch-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-    />
+    <Wraper>
+      <Watch
+        height="100%"
+        width="100%"
+        radius="48"
+        color="#3f51b5"
+        ariaLabel="watch-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    </Wraper>
   );
 };
